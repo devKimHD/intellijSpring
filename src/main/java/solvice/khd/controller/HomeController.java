dbcb379bd4adc7df60ca560998e1374babbbc51f
devKimHD <khd30425@gmail.com>
2022-08-30 14:14:18 +0900

    intellij 로 만든 스프링

---
+++ src/main/java/solvice/khd/controller/HomeController.java
@@ -0,0 +1,14 @@
+package solvice.khd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/hi")
+    public String index(Model model) {
+        model.addAttribute("data", "Hello, Spring from IntelliJ! :)");
+        return "index";
+    }
+}
